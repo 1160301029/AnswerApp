200e7325015cf8da70f489305bd1cf2355e4be8f
Yuxiang-Wei <Yuxiang.Wei.CS@gmail.com>
2019-01-14 21:22:25 +0800

    注册登录

---
+++ app/src/main/java/com/example/answerapp/ui/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.answerapp.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.answerapp.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_test, btn_history;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btn_test = (Button) findViewById(R.id.menu_test);
+        btn_history = (Button) findViewById(R.id.menu_history);
+
+//        btn_test.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this,ChapterListActivity.class);
+//                intent.putExtra("model","Test");
+//                startActivity(intent);
+//            }
+//        });
+//        btn_history.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this,ChapterListActivity.class);
+//                intent.putExtra("model","History");
+//                startActivity(intent);
+//            }
+//        });
+    }
+}
